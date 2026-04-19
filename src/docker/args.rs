1ba8d1a8458bf9e03f7113aab3999a5b1ae238a2
Pawel Lisewski <dev@palekiwi.com>
2026-04-20 00:09:49 +0800

    Add more tests for args

--- src/docker/args.rs
+++ src/docker/args.rs
@@ -43,4 +43,48 @@ mod tests {
             ]
         );
     }
+
+    #[test]
+    fn test_build_run_args_with_opts() {
+        let args = build_run_args(
+            "ocx-nix-daemon",
+            "localhost/ocx-nix-daemon:sha-12345678",
+            vec!["-v".to_string(), "ocx-nix:/nix:rw".to_string()],
+            None
+        );
+
+        assert_eq!(
+            args,
+            vec![
+                "run",
+                "--name",
+                "ocx-nix-daemon",
+                "-v",
+                "ocx-nix:/nix:rw",
+                "localhost/ocx-nix-daemon:sha-12345678"
+            ]
+        );
+    }
+
+    #[test]
+    fn test_build_run_args_with_cmd() {
+        let args = build_run_args(
+            "ocx-nix-daemon",
+            "localhost/ocx-nix-daemon:sha-12345678",
+            vec![],
+            Some(vec!["nix".to_string(), "develop".to_string()])
+        );
+
+        assert_eq!(
+            args,
+            vec![
+                "run",
+                "--name",
+                "ocx-nix-daemon",
+                "localhost/ocx-nix-daemon:sha-12345678",
+                "nix",
+                "develop"
+            ]
+        );
+    }
 }
